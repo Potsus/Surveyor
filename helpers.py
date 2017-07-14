e18e7ae4cefbfb630dff8e81f11f4f97941d9733
Alex Potts <lex.potsus@gmail.com>
2017-07-14 15:41:49 -0400

    added multiple key use and junk data filling

--- helpers.py
+++ helpers.py
@@ -1,37 +1,68 @@
 import json
+import math
+import numpy as np 
 
 def importJson(file):
-	from collections import OrderedDict
-	with open(file) as json_data:
-		return json.load(json_data, object_pairs_hook=OrderedDict)
+    from collections import OrderedDict
+    with open(file) as json_data:
+        return json.load(json_data, object_pairs_hook=OrderedDict)
 
 def stylesToString(styles):
-	stringifiedStyles = list(map(styleString, styles))
-	string = '&'.join(stringifiedStyles)
-	return string
+    stringifiedStyles = list(map(styleString, styles))
+    string = '&'.join(stringifiedStyles)
+    return string
 
 
 def styleString(element):
-	string = 'style=' + objectToString(element, ':', '|') 
-	return string
-		
+    string = 'style=' + objectToString(element, ':', '|') 
+    return string
+        
 
 def objectToString(object, pairSeperator, objectSeperator):
-	#TODO clean this up, break it out into seperate lines
-	return str(objectSeperator).join(['%s%s%s' % (key,pairSeperator, value) for (key, value) in object.items()])
+    #TODO clean this up, break it out into seperate lines
+    return str(objectSeperator).join(['%s%s%s' % (key,pairSeperator, value) for (key, value) in object.items()])
 
 def writeJsonToFile(variable, filename):
-	with open(str(filename), 'w') as outfile:
-		json.dump(variable, outfile, ensure_ascii=False)
+    with open(str(filename), 'w') as outfile:
+        json.dump(variable, outfile, ensure_ascii=False)
 
 
 def getElevation(location):
-	return location['elevation']
+    return location['elevation']
 
 def cleanRow(row):
-	cleanedRow = map(getElevation, row)
-	return cleanedRow
+    cleanedRow = map(getElevation, row)
+    return cleanedRow
 
 def cleanGrid(grid):
-	cleanedGrid = map(cleanRow, grid)
-	return cleanedGrid
+    cleanedGrid = map(cleanRow, grid)
+    return cleanedGrid
+
+def feetToInches(feet):
+    return feet * 12
+
+def feetReadable(dist):
+    feet   = math.floor(dist)
+    inches = round(((dist % 1) * 12), 2)
+
+    #format outa
+    out = ''
+    if feet > 0:
+        out = '%sft ' % feet
+    if inches > 0:
+        out += ' %sin' % inches
+    return out
+
+def scaleBigToSmall(scale, dist):
+    return dist/scale
+
+def roundToEven(f):
+    return int(math.ceil(f / 2.) * 2)
+
+def listColDelete(list, col):
+    for row in list:
+        del row[col]
+
+def listCol(list, col):
+    for row in list:
+        print row[col]
--- survey.py
+++ survey.py
@@ -20,73 +20,187 @@ logging.debug('starting surveyor')
 
 #my stuff
 from helpers import *
-from keys import gmapsApiKey
+from keys import gmapsApiKeys
 
-gmaps = googlemaps.Client(key=gmapsApiKey)
+from calculate import TICK as softTick
+from calculate import NORTH, SOUTH, EAST, WEST
+TICK = softTick * 32
+
+gmaps = googlemaps.Client(key=gmapsApiKeys[0])
+keyNum = 0
 options = importJson('options.json')
 MAP_RESOLUTION = 320
 SCALE = 2
-ELEVATION_RESOLUTION = (MAP_RESOLUTION * SCALE)/1
 
 staticMapUrl = "https://maps.googleapis.com/maps/api/staticmap"
-elevationUrl = 'https://maps.googleapis.com/maps/api/elevation/json'
+ELEVATION_URL = 'https://maps.googleapis.com/maps/api/elevation/json'
 
 #move the styles into a seperate list
 styles = options.pop('styles')
 
 # Geocoding an address
-geocode_result = gmaps.geocode('mosquito island')
+LOCATION = 'virgin islands'
+geocode_result = gmaps.geocode(LOCATION)
 
 lat = geocode_result[0]['geometry']['location']['lat']
 lng = geocode_result[0]['geometry']['location']['lng']
 
 #bounds   = geocode_result[0]['geometry']['bounds']
 viewport = geocode_result[0]['geometry']['viewport']
-#viewport['northeast']['lat'] = 18.532035
-#viewport['northeast']['lng'] = -64.324514
-#viewport['southwest']['lat'] = 18.271421
-#viewport['southwest']['lng'] = -65.105230
 
+#NORTH = viewport['northeast']['lat'] 
+#EAST  = viewport['northeast']['lng']  
+#SOUTH = viewport['southwest']['lat'] 
+#WEST  = viewport['southwest']['lng'] 
 
-latDiff = ((viewport['northeast']['lat'] - viewport['southwest']['lat'])/ELEVATION_RESOLUTION)
-lngDiff = ((viewport['northeast']['lng'] - viewport['southwest']['lng'])/ELEVATION_RESOLUTION)
+#ELEVATION_RESOLUTION = (MAP_RESOLUTION * SCALE)/4
+MAX_REQUESTS = 512
+ELEVATION_RESOLUTION = abs(NORTH - SOUTH) / TICK
+
+ELEVATION_RESOLUTION = roundToEven(ELEVATION_RESOLUTION)
+
+def ticksToResolution(a,b):
+    return roundToEven(abs(a - b) / TICK)
+
+yResolution = ticksToResolution(NORTH, SOUTH)
+xResolution = ticksToResolution(EAST, WEST)
+
+print('fetching grid sized %s x %s is this ok?' % (xResolution, yResolution))
+#sleep(4)
 
 elevationGrid = []
 
+junkSample = json.loads('{"elevation": 0, "location": {"lat": 0, "lng": 0}, "resolution": 0}')
 
-#for y in range (0, ELEVATION_RESOLUTION):
-#	#latitude to sample from should be derived from viewport bounds and the elevation resolution
-#	sampleLat = viewport['southwest']['lat'] + (latDiff * y)
-#	for x in range (0, ELEVATION_RESOLUTION):
-#		sampleLng = viewport['northeast']['lng'] + (lngDiff * x)
-filename = '%s, %s at %s' % (lat, lng, ELEVATION_RESOLUTION)
+
+filename = '%s at %sx%s' % (geocode_result[0]['address_components'][0]['short_name'], xResolution, yResolution)
+#filename = '%s, %s at %s' % (lat, lng, ELEVATION_RESOLUTION)
+
+#def useNextKey():
+#    global keyNum
+#    if (keynum < len(gmapsApiKeys))
+#        keyNum += 1
+#        return true
+#    return false
+
+def useNextKey():
+    global keyNum
+    keyNum += 1
+
+def makeJunkData(samples):
+    junkData = []
+    for i in range(0, samples):
+        junkData.append(junkSample)
+    return junkData
+
+
+def requestLineFragment(lineLat, samples, start):
+    #if we've run out of keys return junk data
+    if (keyNum >= len(gmapsApiKeys)):
+        return makeJunkData(samples)
+
+    lineLng = EAST - (TICK * start)
+
+    queryString  = "%s?path=%s,%s|%s,%s&samples=%s&key=%s" % (ELEVATION_URL, lineLat, lineLng, lineLat, lineLng - (TICK * samples), samples, gmapsApiKeys[keyNum])
+    response     = requests.get(queryString)
+    responseData = json.loads(response.content)
+    if responseData['status'] != 'OK':
+        logging.debug('response error: %s' % responseData['status'])
+        useNextKey()
+        return requestLineFragment(lineLat, samples, start)
+
+    return responseData['results']
+
+
+def getFragmentSamples(desiredSamples):
+    if desiredSamples > MAX_REQUESTS:
+        return getFragmentSamples(desiredSamples/2)
+    return desiredSamples
+
+
+
+
+
 
 if os.path.isfile('raw/' + filename + '.json'):
-	elevationGrid = importJson('raw/' + filename + '.json')
+    elevationGrid = importJson('raw/' + filename + '.json')
 else:
-	for y in range (0, ELEVATION_RESOLUTION):
-		sampleLat    = viewport['northeast']['lat'] - (latDiff * y)
-		queryString  = "%s?path=%s,%s|%s,%s&samples=%s&key=%s" % (elevationUrl, sampleLat, viewport['northeast']['lng'], sampleLat, viewport['southwest']['lng'], ELEVATION_RESOLUTION, gmapsApiKey)
-		response     = requests.get(queryString)
-		responseData = json.loads(response.content)
-		elevationGrid.append(responseData['results'])
-		logging.debug('completed gridline %s' % y)
-		#sleep(0.25)
 
-	writeJsonToFile(elevationGrid, 'raw/' + filename + '.json')
+    #break 
+    maxSamples = getFragmentSamples(xResolution)
+
+    for y in range (0, yResolution):
+
+        sampleLat    = NORTH - (TICK * y)
+        x = 0
+        line = []
+        while(x < xResolution):
+            #make sure you don't go past elevation resolution
+            samplesToRequest = maxSamples
+            if(x + maxSamples > xResolution):
+                samplesToRequest = xResolution - x
+            line.extend(requestLineFragment(sampleLat, samplesToRequest, x))
+            x = x + samplesToRequest
+        elevationGrid.append(line)
+        logging.debug('completed gridline %s of %s' % (y, yResolution))
+
+    writeJsonToFile(elevationGrid, 'raw/' + filename + '.json')
+
+
+
+
+
+    #for y in range (0, ELEVATION_RESOLUTION):
+    #   sampleLat    = NORTH - (TICK * y)
+    #   queryString  = "%s?path=%s,%s|%s,%s&samples=%s&key=%s" % (ELEVATION_URL, sampleLat, EAST, sampleLat, WEST, ELEVATION_RESOLUTION, gmapsApiKeys[keynum])
+    #   response     = requests.get(queryString)
+    #   responseData = json.loads(response.content)
+    #   elevationGrid.append(responseData['results'])
+    #   logging.debug('completed gridline %s of %s' % (y, ELEVATION_RESOLUTION))
+    #   #sleep(0.25)
+
+    #writeJsonToFile(elevationGrid, 'raw/' + filename + '.json')
 
 
 cleanedData = np.array(cleanGrid(elevationGrid))
-#cleanedData[cleanedData < 0] = 0
+np.save('%s cleaned' % filename, cleanedData)
+
+def clipLowerBound(lowerBound):
+    cleanedData = np.clip(cleanedData, lowerBound, cleanedData.max())
+
+#clipLowerBound(cleanedData, -25)
+
 #writeJsonToFile(cleanedData, filename + ' cleaned.json')
 
-imageData = (255*(cleanedData - np.max(cleanedData))/-np.ptp(cleanedData)).astype(int)
+def generateImage(data, suffix=''):
+    imageData = (255*(data - np.max(data))/-np.ptp(data)).astype(int)
 
-#generate heightmap
-heightMap = Image.fromarray(imageData.astype('uint8'))
-heightMap = PIL.ImageOps.invert(heightMap)
-heightMap = heightMap.transpose(Image.FLIP_LEFT_RIGHT)
-heightMap.save('heightmaps/' + filename + '.png')
+    #generate heightmap
+    heightMap = Image.fromarray(imageData.astype('uint8'))
+    heightMap = PIL.ImageOps.invert(heightMap)
+    heightMap = heightMap.transpose(Image.FLIP_LEFT_RIGHT)
+    heightMap.save('heightmaps/' + filename + suffix + '.png')
+
+def generateBoundedImage(data, lower, upper):
+    data = np.clip(data, lower, upper)
+    suffix = (' %s-%s' % (upper, lower))
+    print('generating image with bounds %s' % suffix)
+    generateImage(data, suffix)
+
+def generateCuts(data, min, layerHeight):
+    for i in range(1, int(math.ceil(((data.max() - min) / layerHeight)))):
+        upper = (data.max() - (layerHeight * i-1))
+        lower = (data.max() - (layerHeight * i))
+        
+        generateBoundedImage(data, lower, upper)
+
+
+
+
+generateImage(np.clip(cleanedData, -20, cleanedData.max()))
+
+generateCuts(cleanedData, -25, 50)
+
 
 #Image.new('L', [ELEVATION_RESOLUTION, ELEVATION_RESOLUTION], color=0)
 
@@ -104,7 +218,7 @@ heightMap.save('heightmaps/' + filename + '.png')
 
 
 
-queryString = staticMapUrl+'?'+objectToString(options, '=', '&')+'&'+stylesToString(styles)+'&key='+gmapsApiKey
+queryString = staticMapUrl+'?'+objectToString(options, '=', '&')+'&'+stylesToString(styles)+'&key='+gmapsApiKeys[keyNum]
 
 #first shot at a hardcoded url
 #response = requests.get('https://maps.googleapis.com/maps/api/staticmap?center=mosquito+island&maptype=terrain&scale=2&size=320x320&style=feature:all|element:labels|visibility:off&style=feature:water|element:geometry.stroke|color:0x000000&key=AIzaSyAu4ptX5HdhsJvuK3gDaSwgIk7PpFRoAUg')
