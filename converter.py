2abcc06132d3a13a4df0f73c26af4e523a3cd428
Alex Potts <lex.potsus@gmail.com>
2017-08-09 15:41:40 -0400

    added filesystem converter file

---
+++ converter.py
@@ -0,0 +1,42 @@
+import googlemaps
+from io import BytesIO
+import os.path
+from lazyme.string import color_print
+
+from helpers import *
+from surveyor import *
+config = importYaml('config')
+
+#create google maps interface
+gmaps = googlemaps.Client(key=config['keys'][0])
+
+locations = importYaml('locations')
+
+
+#get the dimensions of the images we're working with
+path = 'heightmaps/'
+files = getVisibleFiles(path)
+print('found:')
+print(files)
+
+for filename in files:
+    #get each file and location data
+    loc, newName = filename.split(' at ')
+    #loc = filename
+    color_print('working on ' + loc, color='yellow', bold=True)
+    location = nameLookup(loc, locations)
+    print('found config ')
+    print(location)
+
+    #print('creating file structure')
+    mapper = surveyor(location['name'], location['bounds']['north'], location['bounds']['south'], location['bounds']['east'], location['bounds']['west'])
+    oldName = (path + filename)
+    newName = (mapper.rootdir + "preview.png")
+    print('moving %s to %s' % (oldName, newName))
+    os.rename(oldName, newName)
+    #mapper.loadFromFile(filename)
+
+
+    color_print('done', color='green')
+    print('')
+    print('')
